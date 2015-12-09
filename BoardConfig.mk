774a3171feece938a36e9d245a1f8c316bcde223
David Vaz Guijarro <vazdavid@icloud.com>
2015-12-09 09:34:43 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,6 +132,9 @@ TARGET_INIT_VENDOR_LIB := libinit_msm
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 TARGET_LIBINIT_DEFINES_FILE := device/oneplus/oneplus2/init/init_oneplus2.cpp
 
+# RIL
+BOARD_RIL_CLASS := ../../../device/oneplus/oneplus2/ril/
+
 # Keymaster app checking
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
